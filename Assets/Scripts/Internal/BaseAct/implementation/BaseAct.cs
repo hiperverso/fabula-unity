86b0c9cc82137e517f7b1c52475d54e6b3dbda75
Vinicius <vinicius@hiperverso.com>
2022-06-22 11:58:28 -0300

    create a first base act structure

---
+++ Assets/Scripts/Internal/BaseAct/implementation/BaseAct.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using Internal.Baseact.api;
+
+namespace Internal.Baseact.implementation
+{
+    public abstract class BaseAct : MonoBehaviour, IAct
+    {
+        public void Load()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void NextAct()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void NextMessage()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void PreviousMessage()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Unload()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
